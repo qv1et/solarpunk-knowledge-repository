4acff067081d696431fbac63f447e337f5e48e79
qv1et <qv1et@riseup.net>
2024-05-30 13:42:48 +0200

    Change font, icon and og-image

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,8 +22,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "TeX Gyre Cursor",
+        body: "TeX Gyre Cursor",
         code: "IBM Plex Mono",
       },
       colors: {
